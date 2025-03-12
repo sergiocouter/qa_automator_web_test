418d1e7dfc60d73b139b74757f147645829e0272
sergiocouter <sergiocouter@gmail.com>
2025-03-11 22:39:23 -0300

    Criada a suite de teste e os casos de teste de uma página de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,9 +1,51 @@
-//funcionalidades
+// Esse comando indica para o Cypress que eu vou usar os comandos dele
+/// <reference types="cypress"/>
 
-//cenario
 
-//cenario
+//funcionalidades - suite de testes
+describe("Login", () => {
 
-//cenario
+    //cenario - caso de teste
+    it("Login com sucesso", () => {
+        //Abrir a aplicação
+        cy.visit("http://www.aprendendotestar.com.br/treinar-automacao.php");
 
-//cenario
+        //Preencher o e-mail
+        cy.get(':nth-child(2) > td > input').type("sergiocouter@gmail.com");
+
+        //Preencher a senha
+        cy.get(':nth-child(4) > td > input').type("Arthur@22");
+
+        //Preencher o nome
+        cy.get(':nth-child(6) > td > input').type("Sérgio Couter")
+
+        // Clicar em entrar
+        cy.get('.btn-info').click();
+
+        //Apagar Cadastro
+        cy.get(':nth-child(2) > :nth-child(5) > a').click();
+    });
+
+    //cenario - caso de teste
+    it("E-mail inválido", () => {
+        cy.visit("http://www.aprendendotestar.com.br/treinar-automacao.php");
+        cy.get(':nth-child(2) > td > input').type("sergiocoutergmail.com");
+        cy.get(':nth-child(4) > td > input').type("Arthur@22");
+        cy.get(':nth-child(6) > td > input').type("Serjão");
+        cy.get('.btn-info').click();
+        cy.get(':nth-child(2) > :nth-child(5) > a').click();
+
+        // cy.get(':nth-child(2) > :nth-child(5) > a').should('have.text', 'E-mail inválido');
+
+    });
+
+    //cenario - caso de teste
+    it("Login", () => {
+
+    });
+
+    //cenario - caso de teste
+    it("Login", () => {
+
+    });
+});
